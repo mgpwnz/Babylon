c1818ffc37afcdd90228b0ba6b7c6fd6f745ca9a
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2024-01-24 13:21:35 +0200

    update

--- babylon.sh
+++ babylon.sh
@@ -37,7 +37,7 @@ packages=("unzip" "gcc" "make" "logrotate" "git" "jq" "lz4" "sed" "wget" "curl" 
 sudo DEBIAN_FRONTEND=noninteractive apt install -yq  "${packages[@]}" &> /dev/null
 # Встановлення Go
                 if ! command -v go; then
-                    VER="1.20.3"
+                    VER="1.20.13"
                     wget "https://golang.org/dl/go$VER.linux-amd64.tar.gz" &> /dev/null
                     sudo rm -rf /usr/local/go &> /dev/null
                     sudo tar -C /usr/local -xzf "go$VER.linux-amd64.tar.gz" &> /dev/null
@@ -52,27 +52,14 @@ git clone https://github.com/babylonchain/babylon.git &> /dev/null
 cd babylon
 git checkout v0.7.2 &> /dev/null
 make build &> /dev/null
-cp $HOME/babylon/build/babylond /usr/local/bin/
+mkdir -p $HOME/.babylond/cosmovisor/genesis/bin
+mv build/babylond $HOME/.babylond/cosmovisor/genesis/bin/
+rm -rf build
+sudo ln -s $HOME/.babylond/cosmovisor/genesis $HOME/.babylond/cosmovisor/current -f
+sudo ln -s $HOME/.babylond/cosmovisor/current/bin/babylond /usr/local/bin/babylond -f
 cd
-#config
-babylond config chain-id bbn-test-2 &> /dev/null
-babylond config keyring-backend test &> /dev/null
-babylond init $MONIKER --chain-id bbn-test-2 &> /dev/null
-#snap
-curl -Ls https://snapshots.kjnodes.com/babylon-testnet/genesis.json > $HOME/.babylond/config/genesis.json
-curl -Ls https://snapshots.kjnodes.com/babylon-testnet/addrbook.json > $HOME/.babylond/config/addrbook.json
-#config
-sed -i -e "s|^seeds *=.*|seeds = \"3f472746f46493309650e5a033076689996c8881@babylon-testnet.rpc.kjnodes.com:16459\"|" $HOME/.babylond/config/config.toml
-
-sed -i -e "s|^minimum-gas-prices *=.*|minimum-gas-prices = \"0.00001ubbn\"|" $HOME/.babylond/config/app.toml
-
-sed -i \
-  -e 's|^pruning *=.*|pruning = "custom"|' \
-  -e 's|^pruning-keep-recent *=.*|pruning-keep-recent = "100"|' \
-  -e 's|^pruning-keep-every *=.*|pruning-keep-every = "0"|' \
-  -e 's|^pruning-interval *=.*|pruning-interval = "19"|' \
-  $HOME/.babylond/config/app.toml
-
+#cosmovisor
+go install cosmossdk.io/tools/cosmovisor/cmd/cosmovisor@v1.5.0
 #service
 echo "
 [Unit]
@@ -95,6 +82,32 @@ babylond tendermint unsafe-reset-all --home $HOME/.babylond --keep-addr-book &> 
 sudo systemctl daemon-reload
 sudo systemctl enable babylon
 sudo systemctl start babylon
+#config
+babylond config chain-id bbn-test-2 
+babylond config keyring-backend test 
+babylond config node tcp://localhost:16457
+babylond init $MONIKER --chain-id bbn-test-2 
+# Download genesis and addrbook
+curl -Ls https://snapshots.kjnodes.com/babylon-testnet/genesis.json > $HOME/.babylond/config/genesis.json
+curl -Ls https://snapshots.kjnodes.com/babylon-testnet/addrbook.json > $HOME/.babylond/config/addrbook.json
+#config
+sed -i -e "s|^seeds *=.*|seeds = \"3f472746f46493309650e5a033076689996c8881@babylon-testnet.rpc.kjnodes.com:16459\"|" $HOME/.babylond/config/config.toml
+
+sed -i -e "s|^minimum-gas-prices *=.*|minimum-gas-prices = \"0.00001ubbn\"|" $HOME/.babylond/config/app.toml
+
+sed -i \
+  -e 's|^pruning *=.*|pruning = "custom"|' \
+  -e 's|^pruning-keep-recent *=.*|pruning-keep-recent = "100"|' \
+  -e 's|^pruning-keep-every *=.*|pruning-keep-every = "0"|' \
+  -e 's|^pruning-interval *=.*|pruning-interval = "19"|' \
+  $HOME/.babylond/config/app.toml
+#custom ports
+sed -i -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:16458\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:16457\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:16460\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:16456\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":16466\"%" $HOME/.babylond/config/config.toml
+sed -i -e "s%^address = \"tcp://localhost:1317\"%address = \"tcp://0.0.0.0:16417\"%; s%^address = \":8080\"%address = \":16480\"%; s%^address = \"localhost:9090\"%address = \"0.0.0.0:16490\"%; s%^address = \"localhost:9091\"%address = \"0.0.0.0:16491\"%; s%:8545%:16445%; s%:8546%:16446%; s%:6065%:16465%" $HOME/.babylond/config/app.toml
+#snapshot load
+curl -L https://snapshots.kjnodes.com/babylon-testnet/snapshot_latest.tar.lz4 | tar -Ilz4 -xf - -C $HOME/.babylond
+[[ -f $HOME/.babylond/data/upgrade-info.json ]] && cp $HOME/.babylond/data/upgrade-info.json $HOME/.babylond/cosmovisor/genesis/upgrade-info.json
+echo -e "\e[32mПеревірити логи\e[0m"
 echo -e "\e[32msudo journalctl -u babylon -f --no-hostname -o cat\e[0m"
 
 }
