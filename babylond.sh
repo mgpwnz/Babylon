9eb95331b5d3c2a7dbb64340119b57048172fef6
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2024-01-27 23:33:06 +0200

    update

---
+++ babylond.sh
@@ -0,0 +1,128 @@
+#!/bin/bash
+# Default variables
+function="install"
+# Options
+option_value(){ echo "$1" | sed -e 's%^--[^=]*=%%g; s%^-[^=]*=%%g'; }
+while test $# -gt 0; do
+        case "$1" in
+        -in|--install)
+            function="install"
+            shift
+            ;;
+        -un|--uninstall)
+            function="uninstall"
+            shift
+            ;;
+        *|--)
+		break
+		;;
+	esac
+done
+install() {
+#монікер
+if [ ! $MONIKER ]; then
+		read -p "Enter Moniker: " MONIKER
+		echo 'export MONIKER='$MONIKER >> $HOME/.bash_profile
+	fi
+. $HOME/.bash_profile
+# Оновлення та встановлення пакетів
+sudo apt-get install unattended-upgrades
+sudo apt install -y curl git jq lz4 build-essential
+# Встановлення Go
+                if ! command -v go; then
+                sudo rm -rf /usr/local/go
+                curl -L https://go.dev/dl/go1.21.6.linux-amd64.tar.gz | sudo tar -xzf - -C /usr/local
+                echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> $HOME/.bash_profile
+                source .bash_profile
+                fi
+# Clone project repository
+cd && rm -rf babylon
+git clone https://github.com/babylonchain/babylon
+cd babylon
+git checkout v0.7.2
+
+# Build binary
+make install
+
+# Set node CLI configuration
+babylond config chain-id bbn-test-2
+babylond config keyring-backend test
+babylond config node tcp://localhost:20657
+
+# Initialize the node
+babylond init "$MONIKER" --chain-id bbn-test-2
+
+# Download genesis and addrbook files
+curl -L https://snapshots-testnet.nodejumper.io/babylon-testnet/genesis.json > $HOME/.babylond/config/genesis.json
+curl -L https://snapshots-testnet.nodejumper.io/babylon-testnet/addrbook.json > $HOME/.babylond/config/addrbook.json
+
+# Set seeds
+sed -i -e 's|^seeds *=.*|seeds = "03ce5e1b5be3c9a81517d415f65378943996c864@18.207.168.204:26656,a5fabac19c732bf7d814cf22e7ffc23113dc9606@34.238.169.221:26656,ade4d8bc8cbe014af6ebdf3cb7b1e9ad36f412c0@testnet-seeds.polkachu.com:20656"|' $HOME/.babylond/config/config.toml
+
+# Set minimum gas price
+sed -i -e 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.001ubbn"|' $HOME/.babylond/config/app.toml
+
+# Set pruning
+sed -i \
+  -e 's|^pruning *=.*|pruning = "custom"|' \
+  -e 's|^pruning-keep-recent *=.*|pruning-keep-recent = "100"|' \
+  -e 's|^pruning-interval *=.*|pruning-interval = "17"|' \
+  $HOME/.babylond/config/app.toml
+
+# Set additional configs
+sed -i 's|^network *=.*|network = "mainnet"|g' $HOME/.babylond/config/app.toml
+
+# Change ports
+sed -i -e "s%:1317%:20617%; s%:8080%:20680%; s%:9090%:20690%; s%:9091%:20691%; s%:8545%:20645%; s%:8546%:20646%; s%:6065%:20665%" $HOME/.babylond/config/app.toml
+sed -i -e "s%:26658%:20658%; s%:26657%:20657%; s%:6060%:20660%; s%:26656%:20656%; s%:26660%:20661%" $HOME/.babylond/config/config.toml
+
+# Download latest chain data snapshot
+curl "https://snapshots-testnet.nodejumper.io/babylon-testnet/babylon-testnet_latest.tar.lz4" | lz4 -dc - | tar -xf - -C "$HOME/.babylond"
+
+# Create a service
+sudo tee /etc/systemd/system/babylond.service > /dev/null << EOF
+[Unit]
+Description=Babylon node service
+After=network-online.target
+[Service]
+User=$USER
+ExecStart=$(which babylond) start
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=65535
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo systemctl daemon-reload
+sudo systemctl enable babylond.service
+
+# Start the service and check the logs
+sudo systemctl start babylond.service
+sudo journalctl -u babylond.service -f --no-hostname -o cat
+
+}
+uninstall() {
+read -r -p "You really want to delete the node? [y/N] " response
+case "$response" in
+    [yY][eE][sS]|[yY]) 
+    cd $HOME
+    sudo systemctl stop babylond.service
+    sudo systemctl disable babylond.service
+    sudo rm /etc/systemd/system/babylond.service
+    sudo systemctl daemon-reload
+    rm -f $(which babylond)
+    rm -rf $HOME/.babylond
+    rm -rf $HOME/babylon
+    echo "Done"
+    cd $HOME
+    ;;
+    *)
+        echo Сanceled
+        return 0
+        ;;
+esac
+}
+# Actions
+sudo apt install wget -y &>/dev/null
+cd
+$function
