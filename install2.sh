13e4356514cd1e3f604999836b3e7e8258458dc1
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2024-02-25 16:41:10 +0200

    update

--- install2.sh
+++ install2.sh
@@ -109,12 +109,12 @@ mv genesis.json ~/.babylond/config/genesis.json
 sed -i -e "s|^minimum-gas-prices *=.*|minimum-gas-prices = \"0.00001ubbn\"|" $HOME/.babylond/config/app.toml
 
 # Set pruning
-#sed -i \
-#  -e 's|^pruning *=.*|pruning = "custom"|' \
-#  -e 's|^pruning-keep-recent *=.*|pruning-keep-recent = "100"|' \
-#  -e 's|^pruning-keep-every *=.*|pruning-keep-every = "0"|' \
-#  -e 's|^pruning-interval *=.*|pruning-interval = "19"|' \
-#  $HOME/.babylond/config/app.toml
+sed -i \
+  -e 's|^pruning *=.*|pruning = "custom"|' \
+  -e 's|^pruning-keep-recent *=.*|pruning-keep-recent = "100"|' \
+  -e 's|^pruning-keep-every *=.*|pruning-keep-every = "0"|' \
+  -e 's|^pruning-interval *=.*|pruning-interval = "19"|' \
+  $HOME/.babylond/config/app.toml
 
 # Set custom ports
 sed -i -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:16458\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:16457\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:16460\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:16456\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":16466\"%" $HOME/.babylond/config/config.toml
