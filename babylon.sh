7aaf9f2768b1b8036c6c6d261c1dbc7c7ce3c44f
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2024-02-26 12:37:54 +0200

    update

---
+++ babylon.sh
@@ -0,0 +1,142 @@
+#!/bin/bash
+# Default variables
+function="install"
+# Options
+option_value(){ echo "$1" | sed -e 's%^--[^=]*=%%g; s%^-[^=]*=%%g'; }
+while test $# -gt 0; do
+        case "$1" in
+        -in|--install)
+            function="install"
+            shift
+            ;;
+        -un|--uninstall)
+            function="uninstall"
+            shift
+            ;;
+	    -up|--update)
+            function="update"
+            shift
+            ;;
+        *|--)
+		break
+		;;
+	esac
+done
+install() {
+#монікер
+if [ ! $MONIKER ]; then
+		read -p "Enter Moniker: " MONIKER
+		echo 'export MONIKER='$MONIKER >> $HOME/.bash_profile
+	fi
+. $HOME/.bash_profile
+# Оновлення та встановлення пакетів
+sudo apt-get install unattended-upgrades
+packages=("unzip" "gcc" "make" "logrotate" "git" "jq" "lz4" "sed" "wget" "curl" "build-essential" "coreutils" "systemd")
+
+# Встановлення пакетів  та з увімкненням автоматичних перезапусків
+sudo DEBIAN_FRONTEND=noninteractive apt install -yq  "${packages[@]}" &> /dev/null
+# Встановлення Go
+                if ! command -v go; then
+                    VER="1.21.6"
+                    wget "https://golang.org/dl/go$VER.linux-amd64.tar.gz" &> /dev/null
+                    sudo rm -rf /usr/local/go &> /dev/null
+                    sudo tar -C /usr/local -xzf "go$VER.linux-amd64.tar.gz" &> /dev/null
+                    rm "go$VER.linux-amd64.tar.gz" &> /dev/null
+                    [ ! -f ~/.bash_profile ] && touch ~/.bash_profile
+                    echo "export PATH=$PATH:/usr/local/go/bin:~/go/bin" >> ~/.bash_profile
+                    source $HOME/.bash_profile
+                fi
+# Clone project repository
+cd $HOME
+rm -rf babylon
+git clone https://github.com/babylonchain/babylon.git
+cd babylon
+git checkout v0.8.3
+
+# Build binaries
+make build
+# Set node configuration
+babylond config chain-id bbn-test-3
+babylond config keyring-backend test
+babylond config node tcp://localhost:20657
+# Initialize the node
+babylond init $MONIKER --chain-id bbn-test-3
+# Download genesis and addrbook files
+curl -L https://snapshots-testnet.nodejumper.io/babylon-testnet/genesis.json > $HOME/.babylond/config/genesis.json
+curl -L https://snapshots-testnet.nodejumper.io/babylon-testnet/addrbook.json > $HOME/.babylond/config/addrbook.json
+# Set seeds
+sed -i -e 's|^seeds *=.*|seeds = "49b4685f16670e784a0fe78f37cd37d56c7aff0e@3.14.89.82:26656,9cb1974618ddd541c9a4f4562b842b96ffaf1446@3.16.63.237:26656"|' $HOME/.babylond/config/config.toml
+
+# Set minimum gas price
+sed -i -e 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.00001ubbn"|' $HOME/.babylond/config/app.toml
+
+# Set pruning
+sed -i \
+  -e 's|^pruning *=.*|pruning = "custom"|' \
+  -e 's|^pruning-keep-recent *=.*|pruning-keep-recent = "100"|' \
+  -e 's|^pruning-interval *=.*|pruning-interval = "17"|' \
+  $HOME/.babylond/config/app.toml
+
+# Set additional configs
+sed -i 's|^network *=.*|network = "signet"|g' $HOME/.babylond/config/app.toml
+
+# Change ports
+sed -i -e "s%:1317%:20617%; s%:8080%:20680%; s%:9090%:20690%; s%:9091%:20691%; s%:8545%:20645%; s%:8546%:20646%; s%:6065%:20665%" $HOME/.babylond/config/app.toml
+sed -i -e "s%:26658%:20658%; s%:26657%:20657%; s%:6060%:20660%; s%:26656%:20656%; s%:26660%:20661%" $HOME/.babylond/config/config.toml
+
+# Download latest chain data snapshot
+curl "https://snapshots-testnet.nodejumper.io/babylon-testnet/babylon-testnet_latest.tar.lz4" | lz4 -dc - | tar -xf - -C "$HOME/.babylond"
+
+
+# Create and start service
+sudo tee /etc/systemd/system/babylon.service > /dev/null << EOF
+[Unit]
+Description=babylon node service
+After=network-online.target
+
+[Service]
+User=$USER
+ExecStart=$(which babylond) start
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=65535
+
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo systemctl daemon-reload
+sudo systemctl enable babylon.service
+cd
+sudo systemctl start babylon.service 
+
+echo "sudo journalctl -u babylon.service -f --no-hostname -o cat"
+
+}
+uninstall() {
+read -r -p "You really want to delete the node? [y/N] " response
+case "$response" in
+    [yY][eE][sS]|[yY]) 
+    cd $HOME
+    sudo systemctl stop babylon.service
+    sudo systemctl disable babylon.service
+    sudo rm /etc/systemd/system/babylon.service
+    sudo systemctl daemon-reload
+    rm -f $(which babylond)
+    rm -rf $HOME/.babylond
+    rm -rf $HOME/babylon
+    echo "Done"
+    cd $HOME
+    ;;
+    *)
+        echo Сanceled
+        return 0
+        ;;
+esac
+}
+update() {
+echo to do
+}
+# Actions
+sudo apt install wget -y &>/dev/null
+cd
+$function
